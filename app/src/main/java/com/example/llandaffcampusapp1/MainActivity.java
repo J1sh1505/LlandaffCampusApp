5ebfa190d4777fed1824566ebffd1f795452917d
DamianZ98 <84047343+DamianZ98@users.noreply.github.com>
2025-02-21 12:49:55 +0000

    First Commit of the app

---
+++ app/src/main/java/com/example/llandaffcampusapp1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.llandaffcampusapp1;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        // Find NavHostFragment
+        NavHostFragment navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            // Set up BottomNavigationView with NavController
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+            // If NavigationUI fails, manually handle tab selection
+            bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+                int id = item.getItemId();
+                if (id == R.id.homeFragment) {
+                    navController.navigate(R.id.homeFragment);
+                    return true;
+                } else if (id == R.id.exploreFragment) {
+                    navController.navigate(R.id.exploreFragment);
+                    return true;
+                } else if (id == R.id.settingsFragment) {
+                    navController.navigate(R.id.settingsFragment);
+                    return true;
+                }
+                return false;
+            });
+        } else {
+            throw new IllegalStateException("NavHostFragment not found!");
+        }
+    }
+}
