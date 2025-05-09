b19df31ace15902854a8644d7828b4279f9443aa
Spuddy10345 <finjones10345@gmail.com>
2025-05-09 12:49:42 +0100

    try merge

--- app/src/main/java/com/example/llandaffcampusapp/BookInductionFragment.java
+++ app/src/main/java/com/example/llandaffcampusapp/BookInductionFragment.java
@@ -1,49 +1,18 @@
 package com.example.llandaffcampusapp;
 
 import android.os.Bundle;
-import android.content.Intent;
-import android.net.Uri;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 public class BookInductionFragment extends Fragment {
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-
-        // Inflate the layout
-        View view = inflater.inflate(R.layout.fragment_book_induction, container, false);
-
-        // Find the button from the inflated view
-        Button openWebsiteButton = view.findViewById(R.id.openWebsiteButton);
-        Button openBlogButton = view.findViewById(R.id.openBlogButton);
-
-        openWebsiteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://www.cardiffmet.ac.uk/study/open-days-and-events/register-for-an-open-day/";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-            }
-        });
-
-        openBlogButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = "https://studentblogs.cardiffmet.ac.uk";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-            }
-        });
-
-        return view;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_induction, container, false);
     }
 }
